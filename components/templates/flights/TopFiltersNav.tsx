07ad0ac1774d119e62753bfafc4b50f3ecfa89d7
omar chatin <omerchatin19@gmail.com>
2023-06-16 20:27:48 +0300

    working on filters

--- components/templates/flights/TopFiltersNav.tsx
+++ components/templates/flights/TopFiltersNav.tsx
@@ -1,37 +1,141 @@
 "use client";
-import { useIsScrollingDown } from "@lib/hooks/scrollDetect";
+
 import React, { useEffect, useState } from "react";
+import Dialog from "@components/elements/dialog/Dialog";
 import { MdOutlineAttachMoney } from "react-icons/md";
 
+type FilterItem = {
+  label: string;
+  value: string;
+};
+
+type FilterProps = {
+  label: string;
+  items: FilterItem[];
+  filtersState: any;
+  onFilterChange: (filterName: string, checked: boolean) => void;
+};
+
+const CheckboxFilter: React.FC<FilterProps> = ({
+  label,
+  items,
+  filtersState,
+  onFilterChange,
+}) => {
+  return (
+    <div className="flex flex-col mt-4">
+      <div className="font-semibold text-gray-700">{label}</div>
+      <div className="flex flex-col mt-1">
+        {items.map((item) => (
+          <label key={item.value} className="flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              className="form-checkbox h-5 w-5 text-blue-600 rounded-full"
+              checked={filtersState[item.value] || false}
+              onChange={(e) => onFilterChange(item.value, e.target.checked)}
+            />
+            <span className="ml-2 text-gray-700">{item.label}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+import { usePathname, useRouter, useSearchParams } from "next/navigation"; // Import the necessary hooks
+
 export default function TopFiltersNav() {
-  const Scrolling = useIsScrollingDown();
+  // Hooks
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [sortBy, setSortBy] = useState<string>("cheapest");
+  const [filters, setFilters] = useState<{ [key: string]: boolean }>({});
+
+  // Next.js hooks
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const handleFilterChange = (filterName: string, checked: boolean) => {
+    setFilters((prev) => ({ ...prev, [filterName]: checked }));
+  };
+
+  const setFiltersInView = () => {
+    const params = searchParams
+      ? new URLSearchParams(searchParams.toString())
+      : null;
+
+    if (params) {
+      const airlines = Object.keys(filters)
+        .filter((filterName) => filters[filterName])
+        .join(",");
+      params.set("airlines", airlines);
+      params.set("sort", sortBy);
+      router.replace(`${pathname}?${params}`);
+    }
+  };
+
+  useEffect(() => {
+    setFiltersInView();
+  }, [filters, sortBy]);
 
   return (
-    <div
-      className={`${
-        Scrolling && " -translate-y-full shadow-none"
-      } md:w-[25em] bg-white shadow-lg md:!translate-y-0 transition-all duration-300 md:!shadow-lg md:p-3 p-2 h-max md:sticky fixed right-0 md:top-28 top-20 noscrollwheel left-0 md:rounded-lg sm:flex-wrap flex gap-2 overflow-x-scroll snap-x scroll-px-2 snap-mandatory`}
-    >
-      <button className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-sm text-zinc-400">
-        <MdOutlineAttachMoney className="pt-0.5" />
-        <span>cheapest</span>
+    <div className="your-container-styles">
+      <button
+        onClick={() => setIsDialogOpen(true)}
+        className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-xl text-zinc-500"
+      >
+        <span>Filter</span>
       </button>
-      <button className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-sm text-zinc-400">
-        <MdOutlineAttachMoney className="pt-0.5" />
-        <span>cheapest</span>
-      </button>
-      <button className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-sm text-zinc-400">
-        <MdOutlineAttachMoney className="pt-0.5" />
-        <span>cheapest</span>
-      </button>
-      <button className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-sm text-zinc-400">
-        <MdOutlineAttachMoney className="pt-0.5" />
-        <span>cheapest</span>
-      </button>
-      <button className="rounded-md flex gap-0.5 items-center border snap-start py-1 px-2 text-sm text-zinc-400">
-        <MdOutlineAttachMoney className="pt-0.5" />
-        <span>cheapest</span>
-      </button>
+      <Dialog open={isDialogOpen} setOpen={setIsDialogOpen}>
+        <div className="w-full sm:w-96 p-2 bg-white rounded-lg flex flex-col">
+          <div className="flex justify-between items-center border-b py-2">
+            <h1 className="font-bold text-xl">All Filters</h1>
+            <button
+              className="font-semibold text-blue-600"
+              onClick={() => setIsDialogOpen(false)}
+            >
+              Clear
+            </button>
+          </div>
+          <div className="flex flex-col mt-4">
+            <span className="text-gray-600 text-sm">Sort By</span>
+            <select
+              name="sort by"
+              className="mt-2 p-2 bg-gray-100 rounded-full focus:ring-blue-500 focus:border-blue-500 w-full shadow-sm"
+              value={sortBy}
+              onChange={(e) => setSortBy(e.target.value)}
+            >
+              {[
+                { label: "Cheapest", value: "cheapest" },
+                { label: "Fastest", value: "fastest" },
+                { label: "Best", value: "best" },
+              ].map((option) => (
+                <option value={option.value} key={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <CheckboxFilter
+            label="Stops"
+            items={[
+              { label: "Non-Stop", value: "nonstop" },
+              { label: "1 Stop", value: "1stop" },
+            ]}
+            filtersState={filters}
+            onFilterChange={handleFilterChange}
+          />
+          <CheckboxFilter
+            label="Airlines"
+            items={[
+              { label: "Egypt Air", value: "egyptair" },
+              { label: "Emirates", value: "emirates" },
+              { label: "Royal Jordanian", value: "royaljordanian" },
+            ]}
+            filtersState={filters}
+            onFilterChange={handleFilterChange}
+          />
+        </div>
+      </Dialog>
     </div>
   );
 }
