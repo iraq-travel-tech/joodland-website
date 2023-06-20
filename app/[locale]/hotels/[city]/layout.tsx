26ea6314c7d526c3c787ea992cb20c4eb47cc995
omar chatin <omerchatin19@gmail.com>
2023-06-21 01:17:18 +0300

    added filters button

--- app/[locale]/hotels/[city]/layout.tsx
+++ app/[locale]/hotels/[city]/layout.tsx
@@ -6,12 +6,12 @@ export default function layout({ children }: { children: React.ReactNode }) {
       <header
         className={`sticky top-0 left-0 transition-all shadow-none z-40 bg-white`}
       >
-        <nav className="relative flex items-center justify-between max-w-6xl px-4 py-5 mx-auto sm:px-6 lg:px-8">
+        <nav className="relative flex items-center justify-between max-w-7xl px-4 py-5 mx-auto sm:px-6 lg:px-8">
           <p className={`text-xl font-bold text-black `}>JoodLand</p>
         </nav>
       </header>
 
-      <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8 pb-10">
+      <div className="max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 pb-10 flex gap-10 py-8">
         {children}
       </div>
     </div>
--- app/[locale]/hotels/[city]/page.tsx
+++ app/[locale]/hotels/[city]/page.tsx
@@ -1,26 +1,104 @@
 "use client";
+
+import Button from "@components/elements/button/Button";
+import Link from "next/link";
+import React, { useEffect, useState, useRef } from "react";
 import { AiFillStar } from "react-icons/ai";
-import React, { useState, useEffect, useRef } from "react";
+import { GoChevronLeft } from "react-icons/go";
 
-export default function page({
-  params,
-}: {
-  params: {
-    city: string;
-  };
-}) {
+export default function page() {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
   return (
-    <div className="pt-10">
-      <div className="font-semibold text-zinc-400">200+ hotels</div>
-      <div className="text-3xl font-bold capitalize">hotels in kirkuk</div>
+    <div className="flex gap-3 w-full">
+      <div
+        className={`bg-black fixed inset-0 z-40 transition-all
+        ${isFilterVisible ? "opacity-50" : "opacity-0 pointer-events-none"}
 
-      <hr className="my-2" />
+      `}
+        onClick={() => setIsFilterVisible(false)}
+      />
+      <div
+        className={`lg:w-3/12 sm:w-5/12 w-7/12 lg:p-0 px-6 lg:pt-0 pt-20 lg:sticky lg:top-24 lg:block lg:right-0 transition-all lg:bg-transparent bg-white lg:h-max h-full z-50 top-0 border-r ${
+          isFilterVisible ? "fixed right-0" : "fixed -right-full"
+        }`}
+      >
+        <div className="text-2xl font-bold underline">Filters</div>
+        <hr className="my-2" />
 
-      <div className="flex flex-col gap-4">
-        <HotelCard />
-        <HotelCard />
-        <HotelCard />
-        <HotelCard />
+        {/* Maximum Price Filter */}
+        <div className="flex flex-col mt-4">
+          <div className="font-semibold">Maximum Price /night</div>
+          {[
+            { label: "50 USD", checked: false },
+            { label: "100 USD", checked: false },
+            { label: "150 USD", checked: false },
+          ].map((i) => (
+            <div key={i.label} className="flex items-center mt-2">
+              <input
+                type="radio"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                defaultChecked={i.checked}
+                name={i.label}
+              />
+              <label
+                htmlFor={i.label}
+                className="ml-3 min-w-0 flex-1 text-gray-500"
+              >
+                {i.label}
+              </label>
+            </div>
+          ))}
+        </div>
+
+        {/* Stars and Ratings Filter */}
+        <div className="flex flex-col mt-4">
+          <div className="font-semibold">Stars</div>
+          {[1, 2, 3, 4, 5].map((star) => (
+            <div key={star} className="flex items-center mt-2">
+              <input
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                name={`star-${star}`}
+              />
+              <label
+                htmlFor={`star-${star}`}
+                className="ml-3 min-w-0 flex-1 text-gray-500"
+              >
+                {"★".repeat(star)}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex-1">
+        <div className="w-max">
+          <Link href="/">
+            <Button bg={"ghost"} startIcon={<GoChevronLeft />}>
+              Go Back
+            </Button>
+          </Link>
+        </div>
+        <div className="flex mt-2 justify-between items-center">
+          <div className="sm:text-2xl text-lg capitalize font-bold">
+            20 stays in Istanbul
+          </div>
+          <button
+            className="lg:hidden text-blue-600 underline"
+            onClick={() => setIsFilterVisible(!isFilterVisible)}
+          >
+            Filters
+          </button>
+        </div>
+        <hr className="mt-2" />
+        <div className="grid gap-3 mt-2 lg:grid-cols-1 md:grid-cols-2 sm:grid-cols-2 grid-cols-1">
+          <HotelCard />
+          <HotelCard />
+          <HotelCard />
+          <HotelCard />
+          <HotelCard />
+        </div>
       </div>
     </div>
   );
@@ -91,15 +169,15 @@ const HotelCard = () => {
   };
 
   return (
-    <div className="flex sm:flex-row flex-col gap-4 p-3 rounded-lg bg-white">
-      <div className="relative rounded-lg overflow-hidden ">
+    <div className="flex flex-col lg:flex-row p-3 rounded-lg bg-white">
+      <div className="relative rounded-lg overflow-hidden lg:w-[15em]">
         <div
           ref={scrollContainerRef}
           onMouseDown={onMouseDown}
           onMouseLeave={() => setIsMouseDown(false)}
           onMouseUp={() => setIsMouseDown(false)}
           onMouseMove={onMouseMove}
-          className="img flex snap-x rounded-lg overflow-x-scroll snap-mandatory sm:h-[14em] h-[14em] min-w-[10em] noscrollwheel scroll-smooth cursor-grab active:cursor-grabbing"
+          className="img flex snap-x rounded-lg overflow-x-scroll snap-mandatory h-[16em] min-w-[10em] lg:h-[12em] noscrollwheel scroll-smooth cursor-grab active:cursor-grabbing"
         >
           {images.map((image, index) => (
             <img
@@ -110,7 +188,7 @@ const HotelCard = () => {
             />
           ))}
         </div>
-        <div className="absolute flex gap-2 z-20 bottom-8 left-1/2 transform -translate-x-1/2">
+        <div className="absolute flex gap-2 z-20 bottom-4 left-1/2 transform -translate-x-1/2">
           {images.map((_, index) => (
             <div
               onClick={() => handleCircleClick(index)}
@@ -123,32 +201,27 @@ const HotelCard = () => {
         </div>
       </div>
 
-      <div className="flex flex-col">
-        <div className="text-zinc-400 text-sm leading-1">
-          hotel in kirkuk, iraq
-        </div>
-        <div className="font-bold capitalize text-2xl text-secondary-800">
+      <div className="flex flex-col lg:mt-0 mt-2 lg:flex-1 lg:ml-4">
+        <div className="font-bold md:text-xl text-lg lg:text-2xl text-secondary-800 capitalize">
           Marjan Hotel
         </div>
-        <div className="text-zinc-400 text-sm sm:text-base">
-          Experience comfort and convenience at Marjan Hotel. Enjoy modern
-          amenities, friendly service, and a prime location for an unforgettable
-          stay.
+        <div className="text-zinc-400 text-sm lg:text-lg line-clamp-2 capitalize">
+          Experience comfort and convenience at Marjan Hotel.
         </div>
-        <div className="mt-auto flex justify-between items-end">
-          <div className="flex items-center gap-2 text-xs">
+        <div className="mt-4 lg:mt-auto flex justify-between items-end">
+          <div className="flex items-center gap-2 text-xs lg:text-md">
             <AiFillStar className="text-yellow-500" size={12} />
             <div className="font-bold">4.5</div>
             <div className="text-zinc-400">(24 reviews)</div>
           </div>
 
           <div className="flex flex-col items-end">
-            <div className="font-bold text-lg">
+            <div className="font-bold text-lg lg:text-xl">
               $212
-              <span className="text-sm font-normal">/night</span>
+              <span className="text-sm font-normal lg:text-lg">/night</span>
             </div>
 
-            <div className="text-zinc-400 text-xs leading-1">
+            <div className="text-zinc-400 text-xs leading-1 lg:text-md">
               $424 for 2 nights
             </div>
           </div>
